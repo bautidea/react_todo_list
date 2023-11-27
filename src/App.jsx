161c4cd15eead6c33658e145f2597198b9c9a039
bautidea <juanbautistadeangelis@gmail.com>
2023-11-27 08:50:58 -0300

    Adding checkbox logic.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,63 @@
 import './styles.css';
+import { useState } from 'react';
 
 function App() {
+  const [newItem, setNewItem] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTodos((currentTodos) => [
+      ...currentTodos,
+      { id: crypto.randomUUID(), title: newItem, completed: false },
+    ]);
+
+    setNewItem('');
+  };
+
+  const toggleTodo = (id, completed) => {
+    setTodos((currentTodos) =>
+      currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+
+        return todo;
+      })
+    );
+  };
+
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
-          <label htmlfor="item">New Item</label>
-          <input type="text" id="item" />
+          <label htmlFor="item">New Item</label>
+          <input
+            type="text"
+            id="item"
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+          />
         </div>
         <button className="btn">Add</button>
       </form>
+
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+              />
+              {todo.title}
+            </label>
+            <button className="btn btn-danger">Delete</button>
+          </li>
+        ))}
       </ul>
     </>
   );
